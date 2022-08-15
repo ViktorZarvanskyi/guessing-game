45d6942887d28e72924061fe2ff6d389aface398
Viktor.Zarvanskyi <viktor.zarvanskyi@gmail.com>
2022-08-15 17:18:05 +0300

    task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minNum;
+        this.maxNum;
+        this.middleNum;
+    }
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+        return this.middleNum = Math.round((this.minNum + this.maxNum) / 2);
     }
 
     lower() {
-
+        this.maxNum = this.middleNum;
     }
 
     greater() {
-
+        this.minNum = this.middleNum;
     }
 }
 
